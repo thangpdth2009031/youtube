08d14f73cba76b8312917c42bcade1d5e377179c
Duc Thang <thangpdth2009031@fpt.edu.vn>
2020-11-29 22:12:36 +0700

    lam youtube

--- javascript.js
+++ javascript.js
@@ -13,42 +13,24 @@ btnAdd.onclick = function () {
             var itemList = document.querySelector('#item-list');
             itemList.innerHTML = '';
             for (var i = 0; i < jsonContent.items.length; i++) {
-                itemList.innerHTML += `<div class="item" onclick="playvideowithID(\`${jsonContent.items[i].id.videoId}\`)">
-                    <div class="item-name">${jsonContent.items[i].snippet.title}</div>
+                itemList.innerHTML += `<div class="item" onclick="playvideowithID(\`${jsonContent.items[i].id.videoId}\`)">                
                     <div class="item-img" >
                         <img src="${jsonContent.items[i].snippet.thumbnails.high.url}" alt="">
                     </div>
+                    <div class="item-name">${jsonContent.items[i].snippet.title}</div>
                 </div>`;
             }
         }
     };
-    xhttp.open('GET', `https://content.googleapis.com/youtube/v3/search?q=${keyword}&type=video&maxResults=9&part=snippet&key=AIzaSyDHPUEaGp5LvX1nEEi0BioIzZqR6YRxNpU`, true);
+    xhttp.open('GET', `https://content.googleapis.com/youtube/v3/search?q=${keyword}&type=video&maxResults=16&part=snippet&key=AIzaSyDcjRc2evkmUt3comSscQteMXyrg3jwCHk`, true);
     xhttp.send();
 }
-
 function playvideowithID(id) {
     modal.style.display = 'block';
-    videoframe.src = `https://www.youtube.com/embed/${id}?autoplau=1`;
+    videoframe.src = `https://www.youtube.com/embed/${id}?autoplau=2`;
 }
 
 myClose.onclick = function (){
     modal.style.display = 'none';
     videoframe.src = '';
-}
-
-var mybutton = document.getElementById("myBtn");
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
-}
-
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
 }
